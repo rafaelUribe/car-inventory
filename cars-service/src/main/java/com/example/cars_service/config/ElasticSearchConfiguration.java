d7643de0a9f6b6b5ab3da4a8415230c9d99e4463
Rafael <rafagallardo75@gmail.com>
2024-08-25 19:28:05 -0600

    elasticsearch dependencies

---
+++ cars-service/src/main/java/com/example/cars_service/config/ElasticSearchConfiguration.java
@@ -0,0 +1,56 @@
+package com.example.cars_service.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "es.app.repository")
+public class ElasticSearchConfiguration {
+
+    @Value("${elasticsearch.endpoint}")
+    private String clusterEndpoint;
+
+    @Value("${elasticsearch.credentials.user}")
+    private String username;
+
+    @Value("${elasticsearch.credentials.password}")
+    private String password;
+
+    @Bean
+    public RestClient restClient() {
+        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        return RestClient.builder(
+                        new HttpHost(clusterEndpoint, 443, "https"))
+                .setHttpClientConfigCallback(httpClientBuilder ->
+                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+                .build();
+    }
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient(RestClient restClient) {
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        return new ElasticsearchClient(transport);
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate(ElasticsearchClient elasticsearchClient) {
+        return new ElasticsearchTemplate(elasticsearchClient);
+    }
+}
--- cars-service/src/main/java/com/example/cars_service/config/WebConfig.java
+++ cars-service/src/main/java/com/example/cars_service/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "api-gateway-production-2fca.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
